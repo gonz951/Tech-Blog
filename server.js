58f19a8e863c6d5f94e5e4d44d65f1af7f9dd9e1
Fel <felgonzalez951@gmail.com>
2024-06-04 12:05:11 -0600

    added handlebar configuration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -20,11 +21,15 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', exphbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.status(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening!'));
-})
+});
