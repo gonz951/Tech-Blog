3ecebb554874d88442b00aa9df659712ab960a85
Fel <felgonzalez951@gmail.com>
2024-06-04 12:36:24 -0600

    added the rest of the sequelize content to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const sequelize = process.env.DB_URL
+    ? new Sequelize(process.env.DB_URL)
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'postgres',
+        }
+    );
+
+module.exports = sequelize;
