035ecb2a76fbe526e3555cfe861b94f622325566
Fel <felgonzalez951@gmail.com>
2024-06-06 16:50:06 -0600

    added logout code content

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,15 @@
-// ! ADD CONTENT 
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
+// * Might be finished
