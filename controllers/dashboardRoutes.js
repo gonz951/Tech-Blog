aed08f7234d3a74e05f1c17c3a1546f95a549a7b
Fel <felgonzalez951@gmail.com>
2024-06-06 19:20:58 -0600

    renamed instances of 'email' in loginFormHandler

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,1 +1,10 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        
+    }
+})
 // ! ADD CONTENT
