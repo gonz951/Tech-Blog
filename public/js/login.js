56104bc198a10000e40d0a3ccdcb26a08ac9ac0c
Fel <felgonzalez951@gmail.com>
2024-06-06 19:02:18 -0600

    removed instances of 'email' from signupFormHandler

--- public/js/login.js
+++ public/js/login.js
@@ -27,10 +27,10 @@ const signupFormHandler = async (event) => {
     const name = document.querySelector('#name-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (name && email && password) {
+    if (name && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ name, password }),
             headers: { 'Content-type': 'application/json' },
         });
 
