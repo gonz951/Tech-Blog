24a5fdc322404a7da30fe2b748fc5868a3acc493
Fel <felgonzalez951@gmail.com>
2024-06-06 19:56:06 -0600

    moved dashboard back to home

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,6 +48,25 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/dashboard');
