efe664052579289b4b4cab11529385064c224610
Fel <felgonzalez951@gmail.com>
2024-06-17 15:28:41 -0600

    mild tweaks

--- controllers/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { Post, User } = require('../models');
-const withAuth = require('../utils/auth');
-
-router.get('/')
-
-module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
             include: [
                 {
                     model: User, 
-                    attributes: ['name'],
+                    attributes: ['username'],
                 },
             ],
         });
@@ -32,7 +32,7 @@ router.get('/post/:id', async (req, res) => {
             include: [
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ['username'],
                 },
             ],
         });
